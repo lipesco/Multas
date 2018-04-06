02a97e0b331e5627df238ce9f25d05b04bc76367
lipesco <filipe.branco@gmail.com>
2018-04-06 20:10:33 +0100

    Introdução de expressões regulares

---
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Multas.Models;
+
+namespace Multas.Controllers
+{
+    public class AgentesController : Controller
+    {
+        //cria um objecto privado que 'referência' a BD
+        private MultasDb db = new MultasDb();
+
+        // GET: Agentes
+        public ActionResult Index()
+        {
+            // em LINQ : db.Agentes.ToList() --> em sql : select * from Agentes;
+            // lista de agentes, presentes na BD
+            return View(db.Agentes.ToList());
+        }
+
+        // GET: Agentes/Details/5
+        /// <summary>
+        /// Apresenta numa listagem os dados de um agente
+        /// </summary>
+        /// <param name="id">identifica o número do agente a pesquisar</param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
+        {
+            // int? id ---> o '?' informa que o parâmetro é de preenchimento facultativo
+            //caso não haja ID, nada é feito
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            // pesquisa os dados do agente, cujo ID foi fornecido
+            Agentes agentes = db.Agentes.Find(id);
+            // valida se foi encontrado algum Agente, se não foi encontrado, nada faz
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            // apresenta na view os dados do agente
+            return View(agentes);
+        }
+
+        // GET: Agentes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agentes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nome,Fotografia,Esquadra")] Agentes agentes)
+        {
+            // ModelState.IsValid --> confronta os dados recebidos como o modelo,
+            // para verificar se o que recebeu é o que deveria ter sido recebido
+            if (ModelState.IsValid)
+            {
+                // adiciona o Agente à estrutura de dados
+                db.Agentes.Add(agentes);
+                // efectuam um commit à BD
+                db.SaveChanges();
+                // redirecciona o utilizador para a página do inicio
+                return RedirectToAction("Index");
+            }
+            // se aqui chegou, é pq aalguma coisa correu mal...
+            // devolvo os dados do agente à view
+            return View(agentes);
+        }
+
+        // GET: Agentes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agentes);
+        }
+
+        // POST: Agentes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nome,Fotografia,Esquadra")] Agentes agentes)
+        {
+            if (ModelState.IsValid)
+            {
+                // update
+                db.Entry(agentes).State = EntityState.Modified;
+                // commit
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agentes);
+        }
+
+        // GET: Agentes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agentes);
+        }
+
+        // POST: Agentes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteNewMethod(int id)
+        {
+            Agentes agentes = db.Agentes.Find(id);
+            db.Agentes.Remove(agentes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,11 +13,18 @@ namespace Multas.Models
             ListaMultas = new HashSet<Multas>();
         }
 
+        [Key]
         public int ID { get; set; } // chave primária
         // dados do Agente
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
+        [RegularExpression("[A-ZÁÂÓÉÍ][a-záéíóúàèìòùâêîôûãõçäëïöüñ]+(( | e | de | da | dos | das | do | d'|-)[A-ZÁÂÓÉÍ][a-záéíóúàèìòùâêîôûãõçäëïöüñ]+){1,3}", ErrorMessage ="Tem de introduzir um {0} num formato válido")]
+        //[StringLength(40,ErrorMessage ="O {0} deve ter um máximo de {1} caracteres")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
         public string Fotografia { get; set; }
         // local de trabalho do Agente
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
+        [RegularExpression("[A-Za-z0-9ç -]+", ErrorMessage = "Tem de introduzir uma {0} num formato válido")]
         public string Esquadra { get; set; }
         // criar uma lista de multas aplicadas pelo agente
         public virtual ICollection<Multas> ListaMultas { get; set; }
